ea4c5d4c34c43e6a78bffda6dc34e2b638edb4c0
Gert Cuykens <gert.cuykens@gmail.com>
2020-07-13 00:36:00 +0200

    main

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/gertcuykens/jwt
 
-go 1.13
+go 1.14
 
-require github.com/google/go-cmp v0.3.1
+require github.com/google/go-cmp v0.5.0
--- main/main.go
+++ main/main.go
@@ -2,29 +2,126 @@ package main
 
 import (
 	"crypto/ed25519"
+	"crypto/x509"
 	"encoding/base64"
+	"encoding/json"
+	"encoding/pem"
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/gertcuykens/jwt"
 )
 
+type myHandler func(w http.ResponseWriter, r *http.Request)
+
+func (h *myHandler) GET(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
-	p := "8080"
-	if "" != os.Getenv("PORT") {
-		p = os.Getenv("PORT")
+
+	var pk ed25519.PrivateKey = func() ed25519.PrivateKey {
+		seed, err := base64.RawURLEncoding.DecodeString(os.Getenv("SEED"))
+		if err != nil {
+			panic(err)
+		}
+		return ed25519.NewKeyFromSeed(seed)
+	}()
+
+	var fn myHandler
+	fn = func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("Authorization")
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		validator := []jwt.Validator{
+			jwt.ValidIssuer(r.Referer()),
+			jwt.ValidAudience([]string{"aud"}),
+			jwt.ValidNotBefore(time.Now()),
+			jwt.ValidIssuedAt(time.Now()),
+			jwt.ValidExpirationTime(time.Now()),
+		}
+
+		auth := jwt.Payload{
+			PublicKey: pk.Public().(ed25519.PublicKey),
+			Validator: validator,
+		}
+
+		err = auth.UnmarshalJSON([]byte(fmt.Sprintf(`"%s"`, cookie.Value)))
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		switch {
+		case r.Method == http.MethodGet:
+			fn.GET(w, r)
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			w.Write([]byte("method not allowed"))
+		}
 	}
 
-	iss := os.Getenv("ORIGIN") + "/public"
-	aud := []string{os.Getenv("ORIGIN") + "/verify"}
-	seed, err := base64.RawURLEncoding.DecodeString(os.Getenv("SEED"))
-	if err != nil {
+	x := http.NewServeMux()
+
+	x.Handle("/", http.HandlerFunc(fn))
+
+	x.HandleFunc("/public", func(w http.ResponseWriter, r *http.Request) {
+		x509PublicKey, err := x509.MarshalPKIXPublicKey(pk.Public().(ed25519.PublicKey))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY", Bytes: x509PublicKey}))
+	})
+
+	x.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		u, p, ok := r.BasicAuth()
+		if !ok || p != "admin" {
+			w.Header().Set("WWW-Authenticate", `Basic realm="`+r.Referer()+`"`)
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+		pl := jwt.Payload{
+			PrivateKey:     pk,
+			Issuer:         r.Referer(),
+			Audience:       []string{"aud"},
+			ExpirationTime: jwt.NumericDate(time.Now().Add(1 * time.Hour)),
+			Subject:        u,
+		}
+		v, err := json.Marshal(pl)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		c := strings.Trim(string(v), `"`)
+		http.SetCookie(w, &http.Cookie{Path: "/", Name: "Authorization", Value: c, HttpOnly: true, SameSite: http.SameSiteStrictMode})
+		w.Header().Set("Location", "/")
+		w.WriteHeader(http.StatusSeeOther)
+	})
+
+	x.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{Path: "/", Name: "Authorization", Value: "", HttpOnly: true, SameSite: http.SameSiteStrictMode, MaxAge: -1})
+		// w.Header().Set("Clear-Site-Data", `"cookies"`)
+		w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
+		w.Header().Set("Location", "/")
+		w.WriteHeader(http.StatusSeeOther)
+	})
+
+	if err := http.ListenAndServe(":8080", x); err != nil {
 		panic(err)
-	}
-	pk := ed25519.NewKeyFromSeed(seed)
-
-	mux := NewServeMux(iss, aud, pk)
-	mux.Handle("/", http.FileServer(http.Dir("./pub")))
-	if err := http.ListenAndServe(":"+p, mux); err != nil {
-		fmt.Fprintln(os.Stderr, err)
 	}
 }
